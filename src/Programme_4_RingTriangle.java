4b25d79e5359737cb9fa56a311db4d209498a729
Paulomi Patel <mital.mital1312>
2023-03-17 14:14:24 +0000

    my homework

---
+++ src/Programme_4_RingTriangle.java
@@ -0,0 +1,27 @@
+public class Programme_4_RingTriangle {
+    /**
+     * /**
+     *  * 4. Write a Java Program to print below triangle
+     *  *
+     *  * *
+     *  * **
+     *  * ***
+     *  * ****
+     *  * *****
+     *  * ******
+     *  */
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+        System.out.println("******");
+
+    }
+
+
+
+
+
+}
